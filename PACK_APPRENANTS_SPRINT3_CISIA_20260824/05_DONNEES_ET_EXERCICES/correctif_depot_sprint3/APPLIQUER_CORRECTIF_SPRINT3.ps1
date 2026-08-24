6b36ca457f0d95b94c933070d4723109a0ee7c65
gsaidani <guillaume.saidani@gmail.com>
2026-08-24 11:20:00 +0200

    Retirer le pack apprenants du suivi git (materiel de cours, pas du code livrable)

--- PACK_APPRENANTS_SPRINT3_CISIA_20260824/05_DONNEES_ET_EXERCICES/correctif_depot_sprint3/APPLIQUER_CORRECTIF_SPRINT3.ps1
+++
@@ -1,7 +0,0 @@
-[CmdletBinding()]
-param(
-    [Parameter()]
-    [string]$RepoPath = "."
-)
-$ErrorActionPreference = "Stop"
-throw "ARCHIVE OBSOLETE - correctif Sprint 3 neutralise. Aucune lecture ni modification du depot n'a ete effectuee. Utilisez CISIA_24082026_Parcours et chargez uniquement le jalon annonce avec scripts/formation/mettre_a_niveau."
